e7fdc32733d9b38c9a8e80acf7fb8e38b8c28d96
Wuhao Chen <c.jim8889@outlook.com>
2019-05-08 20:09:51 +0000

    refactor

--- src/Components/UserInterface.js
+++ src/Components/UserInterface.js
@@ -1,113 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button, Header, Divider, Message } from 'semantic-ui-react';
 import ModalForm from './ModalForm';
 import axios from 'axios';
 
 
-export default function UserInterface(props) {
-    const [user, setUser] = useState({});
-    const [messages, setMessages] = useState([]);
 
-    const msgList = messages.map((msg) => {
-        return (
-            <Message negative={msg.type === 1}>
-                <Message.Header>{msg.text}</Message.Header>
-            </Message>
-        )
-    });
-
-    function handleUserInfoFail() {
-        setMessages([...messages, {type:1, text:"Failed to load user information..."}]);
-    }
-
-    async function changePassword(password) {
-
-        axios.get("http://localhost:5000/api/users/user/password/change", 
-            {
-                crossDomain: true,
-                headers : {
-                    "Authorization" : `Bearer ${props.userToken}`
-                },
-                params : {
-                    "password": password,
-                }
-            }).then((response) => {
-                props.onLogOut();
-            }).catch((err) => {
-                setMessages([...messages, {type:1, text:"Failed to change password"}]);
-        })
-    }
-
-    async function refreshUserToken() {
-        let response = await axios.get("http://localhost:5000/api/users/user/token/refresh",
-            {
-                crossDomain: true,
-                headers: {
-                    "Authorization" : `Bearer ${props.userToken}`
-                },
-                validateStatus: () => true
-            });
-
-        if (response.status === 200) {
-            setUser(Object.assign({}, user, {adminToken: response.data.adminToken, pushToken: response.data.pushToken}));
-        }
-    }
-
-    async function deleteAccount() {
-        let response = await axios.delete("http://localhost:5000/api/users/user",
-        {
-            crossDomain: true,
-            headers: {
-                "Authorization" : `Bearer ${props.userToken}`
-            },
-            validateStatus: () => true
-        })
-
-        if (response.status === 204) {
-            props.onLogOut();
-        }
-    }
-
-    return (
-
-        <div className="user-info">
-            {
-                !user.validated ?
-                <Message negative>
-                    <Message.Header>Unvalidated Account</Message.Header>
-                    <p>Please validate your account</p>
-                    <Button color="blue">Validate</Button>
-                </Message>
-                : null
-            }
-            {msgList}           
-            <Header as="h1">
-            Hi there!
-                <Button floated="right" onClick={props.onLogOut}>Log out</Button>
-            </Header>
-            <Divider hidden />
-            <UserInfo userToken={props.userToken} onFailed={handleUserInfoFail}></UserInfo>
-            <Divider hidden />
-            <Button onClick={refreshUserToken}>Refresh Token</Button>
-            <ModalForm buttonText="Change Password" modalTitle="Password Change" modalContent="Enter your new password below" submitButtonText="Change" onSubmit={changePassword}></ModalForm>
-            <Button color="red" onClick={deleteAccount}>Delete Account</Button>
-        </div>
-    )
-
-}
-
-
-
-class UserInfo extends React.Component {
+export default class UserInterface extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { user: {} };
+        this.state = {
+            user: {}, 
+            messages: []
+        }
 
+
+        this.handleUserInfoFail = this.handleUserInfoFail.bind(this);
+        this.changePassword = this.changePassword.bind(this);
+        this.refreshUserToken = this.refreshUserToken.bind(this);
         this.pullUserInfo = this.pullUserInfo.bind(this);
     }
 
-    pullUserInfo() {
+    componentWillMount() {
+        this.pullUserInfo();
+    }
+    
+    handleUserInfoFail() {
+        this.setState({ messages: [...this.state.messages, {type:1, text:"Failed to load user information..."}]});
+    }
+
+    async pullUserInfo() {
         axios.get("http://localhost:5000/api/users/user", 
             {
                 crossDomain: true,
@@ -119,26 +41,113 @@ class UserInfo extends React.Component {
                     this.setState({user: response.data});
                 }
             }).catch((err) => {
-                this.props.onFailed();                    
+                this.handleUserInfoFail();                
         })
     }
-    
-    componentWillMount() {
-        this.pullUserInfo();
+
+    async changePassword(password) {
+
+        axios.get("http://localhost:5000/api/users/user/password/change", 
+            {
+                crossDomain: true,
+                headers : {
+                    "Authorization" : `Bearer ${this.props.userToken}`
+                },
+                params : {
+                    "password": password,
+                }
+            }).then((response) => {
+                this.props.onLogOut();
+            }).catch((err) => {
+                this.setState({ messages: [...this.state.messages, {type:1, text:"Failed to load user information..."}]});
+        })
+    }
+
+    async refreshUserToken() {
+        let response = await axios.get("http://localhost:5000/api/users/user/token/refresh",
+            {
+                crossDomain: true,
+                headers: {
+                    "Authorization" : `Bearer ${this.props.userToken}`
+                },
+                validateStatus: () => true
+            });
+
+        if (response.status === 200) {
+            let user = {...this.state.user};
+            user.adminToken = response.data.adminToken;
+            user.pushToken = response.data.pushToken;
+            this.setState({user});
+        }
+    }
+
+    async deleteAccount() {
+        let response = await axios.delete("http://localhost:5000/api/users/user",
+        {
+            crossDomain: true,
+            headers: {
+                "Authorization" : `Bearer ${this.props.userToken}`
+            },
+            validateStatus: () => true
+        })
+
+        if (response.status === 204) {
+            this.props.onLogOut();
+        }
     }
 
     render() {
 
         return (
+
+            <div className="user-info">
+                {
+                    !this.state.user.validated ?
+                    <Message negative>
+                        <Message.Header>Unvalidated Account</Message.Header>
+                        <p>Please validate your account</p>
+                        <Button color="blue">Validate</Button>
+                    </Message>
+                    : null
+                }
+                {
+                    this.state.messages.map((msg) => {
+                        return (
+                            <Message negative={msg.type === 1}>
+                                <Message.Header>{msg.text}</Message.Header>
+                            </Message>
+                        )
+                    })
+                }
+                {this.msgList}         
+                <Header as="h1">
+                Hi there!
+                    <Button floated="right" onClick={this.props.onLogOut}>Log out</Button>
+                </Header>
+                <Divider hidden />
+                <UserInfo user={this.state.user}></UserInfo>
+                <Divider hidden />
+                <Button onClick={this.refreshUserToken}>Refresh Token</Button>
+                <ModalForm buttonText="Change Password" modalTitle="Password Change" modalContent="Enter your new password below" submitButtonText="Change" onSubmit={this.changePassword}></ModalForm>
+                <Button color="red" onClick={this.deleteAccount}>Delete Account</Button>
+            </div>
+        )
+    }
+}
+
+class UserInfo extends React.Component {
+    render() {
+
+        return (
             <div>
                 <Header as="h3">Username</Header>
-                <p>{this.state.user.username}</p>
+                <p>{this.props.user.username}</p>
                 <Header as="h3">PushToken</Header>
-                <p>{this.state.user.pushToken}</p>
+                <p>{this.props.user.pushToken}</p>
                 <Header as="h3">AdminToken</Header>
-                <p>{this.state.user.adminToken}</p>
+                <p>{this.props.user.adminToken}</p>
                 <Header as="h3">Receivers Total</Header>
-                <p>{this.state.user.subsribers ? this.state.user.subsribers.length : 0}</p>
+                <p>{this.props.user.subsribers ? this.props.user.subsribers.length : 0}</p>
             </div>
         )
     }
